cf55ed52289758b9e2f5403104b8ad0dd7b43250
Luis-Mencia <luis_mencia@unitec.edu>
2025-03-09 21:26:35 -0600

    Actualizacion clase carro

--- src/alquilercarros/Carro.java
+++ src/alquilercarros/Carro.java
@@ -10,4 +10,72 @@ package alquilercarros;
  */
 public class Carro {
     
+    private String placa;
+    private String marca;
+    private String modelo;
+    private double precio;
+    private boolean disponible;
+    private int edad;
+
+    public Carro(String placa, String marca, String modelo, double precio, boolean disponible) {
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.precio = precio;
+        this.disponible = disponible;
+        this.edad = edad;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+    
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+    
+    
 }
